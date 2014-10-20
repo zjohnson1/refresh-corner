db73a7cdcd4c67c7c5a6ef02190504c53644af9a
zjohnson1 <zjohnson1@engr-134-02.eastern.ewu.edu>
2014-10-20 13:11:59 -0700

    version 0

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  // Your Scripts
+  $('#one').corner();
+  $('#two').corner('30px');
+  $('#three').corner('15px bevel');
+});
